c6f4b0f11c5852dd631ad328bca45adfefa1841d
Alexandre Watanabe <alexandresw@gmail.com>
2016-03-01 18:04:44 -0500

    added user controller and view

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,7 @@
 defmodule Rumbl.PageController do
-  use Rumbl.Web, :controller
+	use Rumbl.Web, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
+	def index(conn, _params) do
+		render conn, "index.html"
+	end
 end
