d48b16daa5dd1205e10d0ad5a4de7d3c5fb1554e
Alexandre Watanabe <alexandresw@gmail.com>
2016-03-05 10:35:25 -0500

    added videos resource

--- web/router.ex
+++ web/router.ex
@@ -25,8 +25,11 @@ defmodule Rumbl.Router do
 
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Rumbl do
-  #   pipe_through :api
-  # end
+  scope "/manage", Rumbl do
+     pipe_through [:browser, :authenticate_user]
+
+     resources "/videos", VideoController
+  end
+
+
 end
